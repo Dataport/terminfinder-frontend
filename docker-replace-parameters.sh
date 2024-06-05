cf2d5da7a6db0303ee878547c6a24dfbfe6afc8f
Schiller, Tjorben <tjorben.schiller@dataport.de>
2024-06-05 10:24:58 +0200

    fix: remove customerId from build cmd

--- docker-replace-parameters.sh
+++ docker-replace-parameters.sh
@@ -6,7 +6,7 @@ LOCALE=$(echo "$LOCALE" || echo "de-DE")
 ADDRESSING=$(echo "$ADDRESSING" || echo "du")
 CUSTOMER_ID=$(echo "$CUSTOMER_ID" || echo "00000000-0000-0000-0000-000000000000")
 
-echo "/docker-entrypoint.d/60-docker-replace-parameters.sh: Replacing Env-Vars with parameters."
+echo "/docker-entrypoint.d/docker-replace-parameters.sh: Replacing Env-Vars with parameters."
 
 find /usr/share/nginx/html -type f -exec sed -i "s/@TITLE@/$TITLE/g" {} \;
 find /usr/share/nginx/html -type f -exec sed -i "s/@LOCALE@/$LOCALE/g" {} \;
@@ -14,4 +14,4 @@ find /usr/share/nginx/html -type f -exec sed -i "s/@ADDRESSING@/$ADDRESSING/g" {
 find /usr/share/nginx/html -type f -exec sed -i "s~@API_URL@~$API_URL~g" {} \;
 find /usr/share/nginx/html -type f -exec sed -i "s~@CUSTOMER_ID@~$CUSTOMER_ID~g" {} \;
 
-echo "/docker-entrypoint.d/60-docker-replace-parameters.sh: Successfully initailized. Starting up nginx."
+echo "/docker-entrypoint.d/docker-replace-parameters.sh: Successfully initailized. Starting up nginx."

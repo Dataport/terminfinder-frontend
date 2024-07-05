908a5fd4eb97e8aea1b7354656597513799bd6c7
hassmal <malte.hass@dataport.de>
2024-07-05 14:42:03 +0200

    feat(DAB-16): change return button to mimic browser return behavior instead of returning to the start

--- src/app/signing-video/sign-language.component.ts
+++ src/app/signing-video/sign-language.component.ts
@@ -1,8 +1,8 @@
-import {Component} from '@angular/core';
+import {Component, inject} from '@angular/core';
 import {YouTubePlayer} from "@angular/youtube-player";
 import {TranslateModule} from "@ngx-translate/core";
 import {RouterLink} from "@angular/router";
-import {NgOptimizedImage} from "@angular/common";
+import {Location, NgOptimizedImage} from "@angular/common";
 
 @Component({
   selector: 'app-sign-language',
@@ -15,19 +15,20 @@ import {NgOptimizedImage} from "@angular/common";
   ],
   template: `
     <div class="d-flex flex-column px-4">
-      <a routerLink="/">
-        <img ngSrc="../../assets/back-blue.svg" aria-hidden="true" width="width" height="height" alt="chevron-left">
+      <a class="d-flex align-items-center" (click)="location.back()">
+        <img ngSrc="../../assets/back-blue.svg" class="back-btn" aria-hidden="true" width="width" height="height" alt="chevron-left">
         {{ 'accessibility.signLanguage.backToTheFuture' | translate }}
       </a>
       <h2 class="mt-4">{{ 'accessibility.signLanguage.header' | translate }}</h2>
       <p>{{ 'accessibility.signLanguage.content' | translate }}</p>
       <h3 class="my-4">{{ 'accessibility.signLanguage.subheader' | translate }}</h3>
-      <youtube-player [videoId]="videoId" [disableCookies]="true" />
+      <youtube-player [videoId]="videoId" [disableCookies]="true"/>
     </div>
   `,
   styleUrl: './sign-language.component.scss'
 })
 export class SignLanguageComponent {
+  protected readonly location = inject(Location);
   // TODO: change video ID to the correct one
   protected readonly videoId = 'zqLEO5tIuYs';
 }

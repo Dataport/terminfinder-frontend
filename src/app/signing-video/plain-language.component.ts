34245eaffbedd3ad3071abad08eea275e407a7da
hassmal <malte.hass@dataport.de>
2024-07-05 14:54:44 +0200

    fix(DAB-17): make back link focusable with correct cursor

--- src/app/signing-video/plain-language.component.ts
+++ src/app/signing-video/plain-language.component.ts
@@ -13,9 +13,9 @@ import {Location, NgOptimizedImage} from "@angular/common";
   ],
   template: `
     <div class="d-flex flex-column px-4">
-      <a class="d-flex align-items-center" (click)="location.back()">
-        <img ngSrc="../../assets/back-blue.svg" aria-hidden="true" width="width" height="height" alt="chevron-left">
-        <span>{{ 'accessibility.plainLanguage.backToTheFuture' | translate }}</span>
+      <a class="back-link" href="#" (click)="location.back()">
+        <img ngSrc="../../assets/back-blue.svg" class="back-btn" aria-hidden="true" width="width" height="height" alt="chevron-left">
+        {{ 'accessibility.plainLanguage.backToTheFuture' | translate }}
       </a>
       <h2 class="mt-4">{{ 'accessibility.plainLanguage.header' | translate }}</h2>
       <p class="multiline">{{ 'accessibility.plainLanguage.content' | translate }}</p>

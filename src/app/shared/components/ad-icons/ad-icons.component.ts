4a2dd0fdab79c47e53ce5bba5332fe59fb602367
Bent Grünberg <bent.gruenberg@dataport.de>
2024-07-26 09:57:55 +0200

    refactor: move ad-icons to its own component

---
+++ src/app/shared/components/ad-icons/ad-icons.component.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-ad-icons',
+  templateUrl: './ad-icons.component.html',
+  styleUrl: './ad-icons.component.scss'
+})
+export class AdIconsComponent {
+  constructor() {
+  }
+}

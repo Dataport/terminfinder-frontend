a2300c34d4b592d4fde70ec84c5b3bd279b617b4
BGruenberg <47741429+BGruenberg@users.noreply.github.com>
2024-08-30 11:01:58 +0200

    refactor: use singleton
    Co-authored-by: Malte Haß <156635416+hassmal@users.noreply.github.com>
    Signed-off-by: BGruenberg <47741429+BGruenberg@users.noreply.github.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -198,7 +198,7 @@ export function createTranslateLoader(http: HttpClient) {
     TosComponent,
     CheckboxFieldComponent,
     NgOptimizedImage,
-    ToastrModule
+    ToastrModule.forRoot(),
   ],
   providers: [
     {

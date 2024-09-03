78a902a2abaade02cee6e678534cc2a9ad4c9e8e
Malte Haß <malte.hass@dataport.de>
2024-09-03 14:52:07 +0200

    feat: style link like a back button

--- src/app/plain-language/plain-language.component.ts
+++ src/app/plain-language/plain-language.component.ts
@@ -13,11 +13,7 @@ import {Location, NgOptimizedImage} from "@angular/common";
   ],
   template: `
     <div class="d-flex flex-column px-4">
-      <a class="back-link" href="#" (click)="location.back()">
-        <img ngSrc="../../assets/back-blue.svg" class="back-btn" aria-hidden="true" width="width" height="height" alt="chevron-left">
-        {{ 'accessibility.plainLanguage.backToTheFuture' | translate }}
-      </a>
-      <h2 class="mt-4">{{ 'accessibility.plainLanguage.header' | translate }}</h2>
+      <h2>{{ 'accessibility.plainLanguage.header' | translate }}</h2>
       <p class="multiline">{{ 'accessibility.plainLanguage.content' | translate }}</p>
       <h3>{{ 'accessibility.plainLanguage.about.header' | translate }}</h3>
       <p class="multiline">{{ 'accessibility.plainLanguage.about.content' | translate }}</p>
@@ -27,8 +23,14 @@ import {Location, NgOptimizedImage} from "@angular/common";
           : {{ 'accessibility.plainLanguage.howto.steps.' + step + '.header' | translate }}</h4>
         <p class="multiline" [innerHTML]="'accessibility.plainLanguage.howto.steps.' + step + '.content' | translate"></p>
       }
+
       <h3>{{ 'accessibility.plainLanguage.declarationOnAccessibility.header' | translate }}</h3>
-      <p class="multiline" [innerHTML]="'accessibility.plainLanguage.declarationOnAccessibility.content' | translate"></p>
+      <p class="multiline mb-4" [innerHTML]="'accessibility.plainLanguage.declarationOnAccessibility.content' | translate"></p>
+      <a class="back-link btn btn-secondary btn-with-image w-100 d-flex justify-content-between" href="#" (click)="location.back()">
+        <img ngSrc="../../assets/back.svg" class="back-btn" aria-hidden="true" width="width" height="2.75rem" alt="chevron-left">
+        {{ 'accessibility.plainLanguage.backToTheFuture' | translate }}
+        <div class="hidden"></div>
+      </a>
     </div>
   `,
   styleUrl: './plain-language.component.scss'

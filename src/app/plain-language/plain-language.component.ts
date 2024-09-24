0ce7f98670b4b7d57c5061251bba9451ac224d23
Grünberg, Bent <bent.gruenberg@dataport.de>
2024-09-24 11:34:49 +0200

    fix: wrong header sizes

--- src/app/plain-language/plain-language.component.ts
+++ src/app/plain-language/plain-language.component.ts
@@ -13,18 +13,18 @@ import {Location, NgOptimizedImage} from "@angular/common";
   ],
   template: `
     <div class="d-flex flex-column px-4">
-      <h2>{{ 'accessibility.plainLanguage.header' | translate }}</h2>
+      <h1>{{ 'accessibility.plainLanguage.header' | translate }}</h1>
       <p class="multiline">{{ 'accessibility.plainLanguage.content' | translate }}</p>
-      <h3>{{ 'accessibility.plainLanguage.about.header' | translate }}</h3>
+      <h2>{{ 'accessibility.plainLanguage.about.header' | translate }}</h2>
       <p class="multiline">{{ 'accessibility.plainLanguage.about.content' | translate }}</p>
-      <h3>{{ 'accessibility.plainLanguage.howto.header' | translate }}</h3>
+      <h2>{{ 'accessibility.plainLanguage.howto.header' | translate }}</h2>
       @for (step of ['0', '1', '2', '3', '4']; track step) {
-        <h4>{{ 'accessibility.plainLanguage.howto.step' | translate }} {{ step }}
-          : {{ 'accessibility.plainLanguage.howto.steps.' + step + '.header' | translate }}</h4>
+        <h3>{{ 'accessibility.plainLanguage.howto.step' | translate }} {{ step }}
+          : {{ 'accessibility.plainLanguage.howto.steps.' + step + '.header' | translate }}</h3>
         <p class="multiline" [innerHTML]="'accessibility.plainLanguage.howto.steps.' + step + '.content' | translate"></p>
       }
 
-      <h3>{{ 'accessibility.plainLanguage.declarationOnAccessibility.header' | translate }}</h3>
+      <h2>{{ 'accessibility.plainLanguage.declarationOnAccessibility.header' | translate }}</h2>
       <p class="multiline mb-4" [innerHTML]="'accessibility.plainLanguage.declarationOnAccessibility.content' | translate"></p>
       <a class="back-link btn btn-secondary btn-with-image w-100 d-flex justify-content-between" href="#" (click)="location.back()">
         <img ngSrc="../../assets/back.svg" class="back-btn" aria-hidden="true" width="width" height="2.75rem" alt="chevron-left">

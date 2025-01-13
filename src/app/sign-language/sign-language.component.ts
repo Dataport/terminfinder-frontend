94a91690a7bc3719b749095543f5a99d7c342c4f
Grünberg, Bent <bent.gruenberg@dataport.de>
2025-01-13 14:29:50 +0100

    feat: add `mediaelement` video player
    - replaces `dGoLive` player
    - urls changed to direct source

--- src/app/sign-language/sign-language.component.ts
+++ src/app/sign-language/sign-language.component.ts
@@ -1,18 +1,16 @@
 import {Component, inject, OnInit} from '@angular/core';
 import {TranslateModule} from "@ngx-translate/core";
-import {RouterLink} from "@angular/router";
 import {Location, NgOptimizedImage} from "@angular/common";
-import {DGoLiveVideoPlayerComponent} from "../shared/components/video-player/d-go-live-video-player.component";
 import {RouteTitleService} from "../shared/services/route-title.service";
+import {MediaelementPlayerComponent} from "../shared/components/mediaelement-player/mediaelement-player.component";
 
 @Component({
   selector: 'app-sign-language',
   standalone: true,
   imports: [
     TranslateModule,
-    RouterLink,
     NgOptimizedImage,
-    DGoLiveVideoPlayerComponent,
+    MediaelementPlayerComponent,
   ],
   template: `
     <div class="d-flex flex-column px-4">
@@ -22,7 +20,7 @@ import {RouteTitleService} from "../shared/services/route-title.service";
       @for (video of videos; track video) {
         <div class="w-100 mb-3">
           <h2 class="my-4">{{ video.titleTranslateString | translate }}</h2>
-          <app-video-player [src]="video.src" [title]="video.titleTranslateString | translate"></app-video-player>
+          <app-mediaelement-player [src]="video.src" [title]="video.titleTranslateString | translate"></app-mediaelement-player>
         </div>
       }
 
@@ -38,16 +36,20 @@ import {RouteTitleService} from "../shared/services/route-title.service";
 export class SignLanguageComponent implements OnInit {
   protected readonly location = inject(Location);
 
-  protected readonly videos = [{
-    titleTranslateString: 'accessibility.signLanguage.videos.0.title',
-    src: 'https://api.dgolive.de/p/105/embedPlaykitJs/uiconf_id/23448421?iframeembed=true&entry_id=0_tig10w13'
-  }, {
-    titleTranslateString: 'accessibility.signLanguage.videos.1.title',
-    src: 'https://api.dgolive.de/p/105/embedPlaykitJs/uiconf_id/23448421?iframeembed=true&entry_id=0_ovo5c099'
-  }, {
-    titleTranslateString: 'accessibility.signLanguage.videos.2.title',
-    src: 'https://api.dgolive.de/p/105/embedPlaykitJs/uiconf_id/23448421?iframeembed=true&entry_id=0_yaxniffj'
-  }];
+  protected readonly videos = [
+    {
+      titleTranslateString: 'accessibility.signLanguage.videos.0.title',
+      src: 'https://api.dgolive.de/p/105/sp/0/playManifest/entryId/0_tig10w13/format/url/flavorParamId/301971/video.mp4'
+    },
+    {
+      titleTranslateString: 'accessibility.signLanguage.videos.1.title',
+      src: 'https://api.dgolive.de/p/105/sp/0/playManifest/entryId/0_ovo5c099/format/url/flavorParamId/301971/video.mp4'
+    },
+    {
+      titleTranslateString: 'accessibility.signLanguage.videos.2.title',
+      src: 'https://api.dgolive.de/p/105/sp/0/playManifest/entryId/0_yaxniffj/format/url/flavorParamId/301971/video.mp4'
+    }
+  ];
 
   constructor(private routeTitle: RouteTitleService) {
   }

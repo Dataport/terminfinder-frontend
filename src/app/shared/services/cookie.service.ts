19421160001986a0ebf966a0fc597cfe258da84f
Grünberg, Bent <bent.gruenberg@dataport.de>
2025-04-14 11:44:43 +0200

    feat: add `CookieService`

---
+++ src/app/shared/services/cookie.service.ts
@@ -0,0 +1,44 @@
+import {Injectable} from "@angular/core";
+import {Logger} from "./logging";
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class CookieService {
+  colors: string[] = [
+    'primary',
+    'primary-lighter-10',
+    'secondary',
+  ];
+
+  constructor(private logger: Logger) {
+  }
+
+  replaceCSSColorsFromCookies(): void {
+    this.logger.info('replaceCSSColorsFromCookies()');
+    console.info('replaceCSSColorsFromCookies()');
+
+    this.colors.forEach((color) => {
+      this.logger.debug('Looking for: ', color);
+      console.debug('Looking for: ', color);
+
+      const colorValue = this.getCookie(color);
+      if (colorValue) {
+        this.logger.debug('Cookie found, value is: ', colorValue);
+        console.debug('Cookie found, value is: ', colorValue);
+        document.documentElement.style.setProperty(`--${color}`, colorValue);
+      }
+    });
+  }
+
+  private getCookie(name: string): string | null {
+    // 1. cookie string trennen "foo=bar; variable=value; etc=123"
+    // 2. value für regex escapen
+    // 3. value auslesen
+    const matches = document.cookie.match(new RegExp(
+      '(?:^|; )' + name.replace(/([$?*|{}\[\]\\\/+^])/g, '\\$1') + '=([^;]*)'
+    ));
+    return matches ? decodeURIComponent(matches[1]) : null;
+  }
+}
